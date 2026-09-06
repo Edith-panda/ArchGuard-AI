72feb4726bb77597f257a80d52b4ae44a981dfa2
Sakure_Yashasvi_0566 <yashasvisakure@gmail.com>
2026-09-06 21:20:13 +0530

    feat: mount remediation approval and MCP dock

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './RemediationActions.css'
+import './RemediationDock.css'
 import App from './App.jsx'
+import RemediationDock, { installAssistantResponseBridge } from './RemediationDock.jsx'
+
+installAssistantResponseBridge()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <>
+      <App />
+      <RemediationDock />
+    </>
   </StrictMode>,
 )
